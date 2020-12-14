2d0be958c85aefebc25c72da1192a0f0c4623d92
takuya hakiri <tyapi4@yahoo.co.jp>
2020-12-15 00:24:46 +0900

    new:api-test

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,9 @@
+require 'resolv'
+
+class PostsController < ApplicationController
+  
+  def index
+    ip_addr_str = Resolv::DNS.new(:nameserver=>'ns1.google.com').getresources("o-o.myaddr.l.google.com", Resolv::DNS::Resource::IN::TXT)[0].strings[0]
+    render json: { msg: "API呼び出し成功。本サーバーのIPアドレスは#{ip_addr_str}です" }
+  end
+end
