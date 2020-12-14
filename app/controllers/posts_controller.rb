daad0b2de7c25b8101f2266949015ced588b2418
takuya hakiri <tyapi4@yahoo.co.jp>
2020-12-15 07:45:20 +0900

    init unicorn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,6 @@ class PostsController < ApplicationController
   
   def index
     ip_addr_str = Resolv::DNS.new(:nameserver=>'ns1.google.com').getresources("o-o.myaddr.l.google.com", Resolv::DNS::Resource::IN::TXT)[0].strings[0]
-    render json: { msg: "API呼び出し成功。本サーバーのIPアドレスは#{ip_addr_str}です" }
+    render json: { msg: "API呼び出し成功。本AWSサーバーのIPアドレスは#{ip_addr_str}です" }
   end
 end
